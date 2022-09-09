8b13b5db00b764a7ba5f28151ccdd2d9f539b252
vitaliy <zzzvitzzz4@gmail.com>
2022-09-10 00:16:14 +0300

    feat: gitattributes

--- bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++ bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001662757764';
-$dateexpire = '001662761424';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1662757823;}';
+$datecreate = '001662758118';
+$dateexpire = '001662761778';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1662758174;}';
 return true;
 ?>
